1bdc243dfd1a29747d1fb34015ea12b9c6c1711b
bodhisrikar <tsrikar187@gmail.com>
2020-11-17 21:03:10 -0500

    navigating to game fragment from title fragment using navcontroller

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
 /**
@@ -17,6 +18,9 @@ class TitleFragment : Fragment() {
     private lateinit var titleFragmentBinding: FragmentTitleBinding
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         titleFragmentBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_title, container, false)
+        titleFragmentBinding.playButton.setOnClickListener {
+            it.findNavController().navigate(R.id.action_titleFragment_to_gameFragment)
+        }
         return titleFragmentBinding.root
     }
 }
